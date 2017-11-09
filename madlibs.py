aee117ef10a04e712c34d4a13a1194bd6e09a515
Footballboy007 <33520725+Footballboy007@users.noreply.github.com>
2017-11-09 09:53:20 -0500

    initial

---
+++ madlibs.py
@@ -0,0 +1,53 @@
+### Madlibs ###
+### source: https://i.pinimg.com/736x/41/a6/c0/41a6c07c0984682b69a98749edb65c3b--funny-mad-libs-pizza-pizza.jpg
+
+print("Give me a adjective")
+noun1= input()
+
+
+print("Give me a nationality")
+adj1= input()
+
+print("Give me a person")
+name1= input()
+
+print("Give me a noun")
+noun2= input()
+
+print("Give me a adjective")
+adj2= input()
+
+print("Give me a noun")
+noun3= input()
+
+print("Give me a adjective")
+adj3= input()
+
+print("Give me a adjective")
+adj4= input()
+
+
+
+print("Give me a plural noun")
+pnoun1= input()
+
+print("Give me a noun")
+noun4= input()
+
+print("Give me a number")
+num1= input()
+
+print("Give me a shape")
+shape1= input()
+
+print("Give me a food")
+food1= input()
+
+print("Give me a food")
+food2= input()
+
+print("Give me a number")
+num2= input()
+
+### Start ###
+print("Pizza was invented by a " + noun1 + " " + adj1 + " chef named " + name1 + ". To make a pizza, you need to take a lump of" + noun2 + ", and make a thin, round" + adj2 + " " + noun3 + ". Then you cover it with " + adj3 + " sauce, " + adj4 + " cheese, and fresh chopped " + pnoun1 + ". Next you have to bake it in a very hot" + noun4 + ". When it is done, cut it into" + num1 + " " + shape1 + ". Some kids like " + food1 + "pizza the best, but my favorite is" + food2 + " pizza. If I could, I would eat pizza" + num2 + " times a day!") 
