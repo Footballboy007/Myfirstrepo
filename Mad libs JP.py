cc1146a3fd51f7750fba3d111fb6b83df9dc66b5
Footballboy007 <33520725+Footballboy007@users.noreply.github.com>
2017-11-16 10:11:45 -0500

    Add files via upload

---
+++ Mad libs JP.py
@@ -0,0 +1,59 @@
+import time 
+
+print("Write a noun")
+noun1 = input()
+
+print("write a person in the room")
+personinroom1 = input ()
+
+print("Write a verb")
+verb1 = input()
+
+print ("Write a Part of the body (plural)")
+partofthebodyplural1 = input()
+
+print ("Write an adjective")
+adjective1 = input()
+
+print ("Write another noun")
+noun2 = input()
+
+print ("Write another noun")
+noun3 = input()
+
+print ("Write a plural noun")
+pluralnoun1 = input()
+
+print ("Write a liquid")
+typeofliquid1 = input()
+
+print ("Write another adjective")
+adjective2 = input()
+
+print ("Write another noun")
+noun4 = input()
+
+print ("Write another noun")
+noun5 = input()
+
+print ("Write another noun")
+noun6 = input()
+
+print ("Write another plural noun")
+pluralnoun2 = input()
+
+print ("Write a person which is in the room which is a female")
+personinroomfemale1= input()
+
+print ("Write another noun")
+noun7 = input()
+
+print ("Write a plural part of the body")
+partofthebodyplural2 = input()
+
+
+### MAD LIB ###
+### https://www.google.com/search?rlz=1C1GCEA_enUS752US752&biw=1517&bih=735&tbm=isch&sa=1&ei=LiL7WYjLOpTmjwTNmbOYAw&q=mad+libs&oq=mad+libs&gs_l=psy-ab.3...41094.44741.0.44998.9.9.0.0.0.0.0.0..1.0....0...1.1.64.psy-ab..8.0.0.0...270.jy6ioRKMs50#imgrc=sGc1U7F2Yy1WbM:###
+
+print(" It was thanksgiving, and the sccent of succulent roast " + noun1 + " wafted through my house." + personinroom1 + " it's time to " + verb1 + " my mother called. I could not wait to get my" + partofthebodyplural1 + " on that " + adjective1 + " Thanksgiving meal. My family sat around the dinning-room " + noun2 + ". The table was laid out with every kind of " + noun3 + " imaginable. There was a basket of hot buttered " + pluralnoun1 + " and glasses of sparkling " + typeofliquid1 + ". The " + adjective2 + "turkey sat, steaming next to a tuteen of " + noun4 + " gravy. A bowl of ruby-red " + noun5 + " sauce, a sweet-" + noun6 + "casserol, and a dish of mashed " + pluralnoun2 + " tempted my taste buds. but the dish I looked forward to most was Grandma " + personinroomfemale1 + "'s famous " + noun7 + " pie. Thanksgiving is my favorite holiday, " + partofthebodyplural2 + " down." )  
+time.sleep(100)
